ae46b9102db5b355eed4c0c83d590d8f3f6c1b98
awais <awaishu.se@gmail.com>
2024-10-08 18:29:34 +0500

    circuit breaker, retry, rate limter implemented

--- UserService/src/main/java/com/user/service/UserService/controllers/UserController.java
+++ UserService/src/main/java/com/user/service/UserService/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.user.service.UserService.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.user.service.UserService.entities.User;
 import com.user.service.UserService.services.UserService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+
 @RestController
 public class UserController {
 	
 	@Autowired
 	UserService userService;
+	
+	private Logger logger = LoggerFactory.getLogger(getClass());
 	
 	@PostMapping("/users")
 	public ResponseEntity<User> createUser(@RequestBody User user) {
@@ -28,8 +35,23 @@ public class UserController {
 	}
 	
 	@GetMapping("/users/{userId}")
+	@CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelBreakerFallback")
+	@RateLimiter(name = "userRatingLimiter", fallbackMethod = "ratingHotelBreakerFallback")
 	public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
 		User user = userService.getUser(userId);
+		return ResponseEntity.ok(user);
+	}
+	
+	public ResponseEntity<User> ratingHotelBreakerFallback(String userId, Exception ex) {
+		logger.info("Fallback is executed because service is down: ", ex.getMessage());
+		
+		User user = User
+				.builder()
+				.email("dummy@dummy.com")
+				.name("dummy")
+				.about("This dummay user is created because some other required service is down")
+				.userId("0000")
+				.build();
 		return ResponseEntity.ok(user);
 	}
 	
